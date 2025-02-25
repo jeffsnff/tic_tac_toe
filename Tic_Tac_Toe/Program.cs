024a44e2b64aec25ee6b7da3aba259be90cb5d61
Jeffrey Seneff <jeffsnff@gmail.com>
2025-02-25 05:11:00 -0500

    Gives player the ability to select a location on the board

--- Tic_Tac_Toe/Program.cs
+++ Tic_Tac_Toe/Program.cs
@@ -8,8 +8,46 @@ namespace Tic_Tac_Toe
     {
       Player playerOne = new Player(Convert.ToChar("X"));
       Player playerTwo = new Player(Convert.ToChar("O"));
-
       Grid board = new Grid(9);
+      Boolean gameOver = false;
+      byte turn = 1;
+      
+      Intro();
+      while (!gameOver)
+      {
+        byte playerMove;
+        board.DrawBoard();
+        if (turn % 2 == 0)
+        {
+          Console.Write($"Player {playerTwo.name} choose a location: ");
+          playerMove = Convert.ToByte(Console.ReadLine());
+          if (playerMove < 1 || playerMove > 9)
+          {
+            Console.WriteLine($"{playerMove} is an invalid selection.\nPlease try again.");
+            continue;
+          }
+          board.UpdateBoard(playerMove, playerTwo.name);
+        }
+        else
+        {
+          Console.Write($"Player {playerOne.name} choose a location: ");
+          playerMove = Convert.ToByte(Console.ReadLine());
+          if (playerMove < 1 || playerMove > 9)
+          {
+            Console.WriteLine($"{playerMove} is an invalid selection.\nPlease try again.");
+            continue;
+          }
+          board.UpdateBoard(playerMove, playerOne.name);
+        }
+        turn++;
+      }
+
+    }
+
+    private static void Intro()
+    {
+      Console.Clear();
+      Console.WriteLine("Welcome to Tic Tac Toe\n\n");
     }
   }
 }
