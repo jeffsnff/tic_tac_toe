f26cb6e88ee9f80684ea5178eaf330b842e7e17b
Jeffrey Seneff <jeffsnff@gmail.com>
2025-02-28 06:08:17 -0500

    removed positions from Player class since it was not used

--- Tic_Tac_Toe/Player.cs
+++ Tic_Tac_Toe/Player.cs
@@ -5,12 +5,10 @@ namespace Tic_Tac_Toe
   internal class Player
   {
     public readonly Char name;
-    public int[] positions;
 
     public Player(Char playerName)
     {
       this.name = playerName;
     }
-
   }
 }
